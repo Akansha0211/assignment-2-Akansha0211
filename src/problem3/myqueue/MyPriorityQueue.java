5ee216a393532b4427729450fe1d4246744fd0b2
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-03-27 16:03:52 +0530

    declare variables of MyPriorityQueue class

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,16 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+
+import java.util.Queue;
+
 public class MyPriorityQueue {
+    // number of elements on queue
+    private int n;
+    //beginning of queue
+    private Node first;
+    //end of queue
+    private Node last;
+
 }
