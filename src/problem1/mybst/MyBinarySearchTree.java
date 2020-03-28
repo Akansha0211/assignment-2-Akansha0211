b1d6438efe433a28d17d8f6ee25545389a74e33d
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-03-28 13:26:22 +0530

    Create getRoot() and setRoot method

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -26,6 +26,13 @@ public class MyBinarySearchTree {
         }
 
         }
+        public TreeNode getRoot(){
+        return root;
+        }
+        public void setRoot(TreeNode root){
+        this.root = root;
+        }
+
 
     }
 
