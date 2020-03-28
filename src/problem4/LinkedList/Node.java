f1be7307c2b1bf17892987a2ed2d96a87f2a44b2
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-03-28 13:02:19 +0530

    Creating getter and setter

--- src/problem4/LinkedList/Node.java
+++ src/problem4/LinkedList/Node.java
@@ -7,5 +7,17 @@ public class Node {
         this.item = item;
         this.next = null;
     }
+    public int getItem(){
+        return item;
+    }
+    public Node getNext(){
+        return next;
+    }
+    public void setItem(int item){
+        this.item = item;
+    }
+    public void setNext(Node next){
+        this.next = next;
+    }
 
 }
