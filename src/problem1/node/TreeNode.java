2ac4b7bb90ef7ef361dfde6e2ddd140d3a4143f3
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-03-28 13:35:46 +0530

    Create getter and setters for TreeNode class

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -17,5 +17,27 @@ public class TreeNode {
         this.right = null;
     }
 
+    public int getData() {
+        return data;
+    }
 
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
