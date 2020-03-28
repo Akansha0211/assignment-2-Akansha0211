acf873973ce22c689580fd1cfa560cd3e23793b5
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-03-28 13:46:19 +0530

    Create MyBinarySearchTree object

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,15 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        bst.insert(15);
+        bst.insert(20);
+        System.out.println("Left children of the BST ");
+        System.out.println("No of parents not having left children");
+       // System.out.println(bst.getCount);
+    }
 }
