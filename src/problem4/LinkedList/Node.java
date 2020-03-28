16987eebcb9bb38b6b2acd22cb65ce04e814b2ff
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-03-28 13:01:50 +0530

    Creating Node Class in LinkedList package

---
+++ src/problem4/LinkedList/Node.java
@@ -0,0 +1,11 @@
+package problem4.LinkedList;
+
+public class Node {
+    private  int item;
+    private Node next;
+    public Node(int item){
+        this.item = item;
+        this.next = null;
+    }
+
+}
